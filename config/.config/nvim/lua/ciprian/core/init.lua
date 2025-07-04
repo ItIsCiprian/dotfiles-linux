108a35bda89a077fc603547ca562a233affa22a0
Ionut Ciprian Anescu <ionutcipriananescu@gmail.com>
2025-07-04 15:16:40 +0300

    Initial commit with .config

---
+++ config/.config/nvim/lua/ciprian/core/init.lua
@@ -0,0 +1,14 @@
+require("ciprian.core.options")
+require("ciprian.core.keymaps")
+
+vim.opt.clipboard = "unnamedplus"
+
+-- [[ Highlight on yank ]]
+local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+vim.api.nvim_create_autocmd("TextYankPost", {
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+	group = highlight_group,
+	pattern = "*",
+})

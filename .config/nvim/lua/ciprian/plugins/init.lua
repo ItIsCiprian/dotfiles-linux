20fb820fe10b74c597a2eb9eebf6bb232bfc53a8
Ionut Ciprian Anescu <ionutcipriananescu@gmail.com>
2025-07-10 21:07:43 +0300

    nvim coach add

--- .config/nvim/lua/ciprian/plugins/init.lua
+++ .config/nvim/lua/ciprian/plugins/init.lua
@@ -1,1 +1,14 @@
-return {}
+return {
+  {
+    "shahshlok/vim-coach.nvim",
+    dependencies = {
+      "folke/snacks.nvim",
+    },
+    config = function()
+      require("vim-coach").setup()
+    end,
+    keys = {
+      { "<leader>?", "<cmd>VimCoach<cr>", desc = "Vim Coach" },
+    },
+  },
+}
